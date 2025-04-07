58bfe78727f3fa7da3d989b1e6ae555aaa1685f3
Gabriele Martinez <gabimarti099@hotmail.com>
2025-04-07 19:31:25 -0300

    Adição de novas telas

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import Conta from './screens/Conta';
+import Multas from './screens/Multas';
+import DetalhesMulta from './screens/DetalhesMulta';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +15,9 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Conta" component={Conta} />
+        <Stack.Screen name="Multas" component={Multas} />
+        <Stack.Screen name="DetalhesMulta" component={DetalhesMulta} />
       </Stack.Navigator>
     </NavigationContainer>
   );
